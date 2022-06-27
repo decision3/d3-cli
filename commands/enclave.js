ed4f202aca8e47e9208736a3e8bb54a0811134c1
abhinit <abhinit.ambastha@gmail.com>
2022-06-27 13:41:09 +0800

    deployed updated

--- commands/enclave.js
+++ commands/enclave.js
@@ -1,33 +1,62 @@
 import got from "got"
 import fs from "fs"
 import { get_url } from "./utils.js"
+import ora from 'ora';
 
 export async function configure(configFile) {
     try {
-        var res = await got.post(get_url('enclave/configure'), {
+        var spinner = ora({
+            text: 'Configuring container',
+            spinner:'arc',
+            indent: 2
+        }).start();
+        await got.post(get_url('enclave/configure'), {
             json: JSON.parse(fs.readFileSync(configFile))
-        }).json()
-        console.log(res.response);
+        })
+        .json()
+        .then(res => {
+            spinner.succeed(res.response);
+        });
      } catch (error) {
-        console.log(error.response.body);   
+        console.log(error);   
         return error
      }
 }
 
 export async function deploy() {
     try {
-        
+        var spinner = ora({
+            text: 'Creating container',
+            spinner:'arc',
+            indent: 2
+        }).start();
         await got.post(get_url('enclave/image'))
         .json()
-        .then(res => console.log(res.response))
+        .then(res => spinner.succeed(res.response+"  "))
+        .then(() => {
+            spinner = ora({
+                text: 'Deploying enclave',
+                spinner:'arc',
+                indent: 2
+            }).start()
+        });
 
         await got.post(get_url('enclave/build'))
         .json()
-        .then(res => console.log(res.response))
+        .then(res => spinner.succeed(res.response))
+        .then(() => {
+            spinner = ora({
+                text: 'Starting enclave',
+                spinner:'arc',
+                indent: 2
+            }).start()
+        });
         
         await got.post(get_url('enclave/run'))
         .json()
-        .then(res => console.log(res.response))
+        .then(res => spinner.succeed(res.response));
+
+        process.exit(0);
 
      } catch (error) {
         return error;
